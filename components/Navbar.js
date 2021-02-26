a2c2c511449ab35fce59a5b5b2b7dbbef931225a
davidmichaelm <davemarquardt@gmail.com>
2021-02-26 09:57:49 -0600

    Make navbar responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,25 @@
-import styles from "../styles/Navbar.module.css";
+import styles from "../styles/Navbar.module.scss";
 
 export default function Navbar() {
     return (
-        <nav className={styles.nav}>
-            <div className={styles.navItem + " " + styles.logo}>
-                <img src="/logo.png" height="40" alt="DM Logo"/>
-            </div>
+        <div className={styles.navWrapper}>
+            <nav className={styles.nav}>
+                <div className={styles.navItem + " " + styles.logo}>
+                    <img src="/logo.png" height="40" alt="DM Logo"/>
+                </div>
 
-            <div className={styles.navItem}>
-                <a href="#">Contact</a>
-            </div>
+                <div className={styles.navItem}>
+                    <a href="#">Contact</a>
+                </div>
 
-            <div className={styles.navItem}>
-                <a href="#">Resume</a>
-            </div>
+                <div className={styles.navItem}>
+                    <a href="#">Resume</a>
+                </div>
 
-            <div className={styles.navItem}>
-                <a href="https://www.linkedin.com/in/david-marquardt-600005197/">LinkedIn</a>
-            </div>
-        </nav>
+                <div className={styles.navItem}>
+                    <a href="https://www.linkedin.com/in/david-marquardt-600005197/">LinkedIn</a>
+                </div>
+            </nav>
+        </div>
     );
 }
