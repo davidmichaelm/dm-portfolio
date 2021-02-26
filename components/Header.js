0364de85ccc7481897072ea8ed05d120918450e4
davidmichaelm <davemarquardt@gmail.com>
2021-02-26 17:34:14 -0600

    Add Header section

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import styles from "../styles/Header.module.scss";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerLeft}>
+                <h1>Hi, I'm David,</h1>
+                <h1 className={styles.subheading}>Software Developer</h1>
+                <a href="#" className="button">Contact me today!</a>
+            </div>
+            <div className={styles.headerRight}>
+                <p>
+                    I'm a driven and resourceful junior developer based in <strong>Milwaukee, WI</strong>.
+                </p>
+                <p>
+                    Front-end development with <strong>Vue</strong>, <strong>React</strong>, and other modern tech are the most interesting to me, but I'm also proficient with <strong>.Net Core</strong> back-ends.
+                </p>
+                <p>
+                    Currently studying Web and Software Development full-time at <a href="https://www.wctc.edu">Waukesha County Technical College</a>, and on the lookout for my next opportunity.
+                </p>
+            </div>
+        </header>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div>
             <Head>
                 <title>David Marquardt</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
             <Navbar/>
+
+            <Header></Header>
+
             <main className={styles.main}>
 
             </main>
