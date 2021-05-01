e2b85fa8547e1a8964a38396915f6c6f250221c7
David Marquardt <davemarquardt@gmail.com>
2021-05-01 15:22:49 -0500

    Open Graph tags and image

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,11 @@ export default function Home() {
                 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
                 <meta name="msapplication-TileColor" content="#da532c"/>
                 <meta name="theme-color" content="#ffffff"/>
+
+                <meta property='og:title' content='David Marquardt Portfolio'/>
+                <meta property='og:image' content='https://davidmarquardt.dev/logo-lg.png'/>
+                <meta property='og:description' content="Showcase of some of David Marquardt's top projects."/>
+                <meta property='og:url' content='https://davidmarquardt.dev'/>
             </Head>
 
             <Navbar />
